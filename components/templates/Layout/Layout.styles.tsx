56fce108ee64a88170388d03839dd79c3dca3809
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-07-29 11:06:08 +0900

    style: Layout overflow 처리

--- components/templates/Layout/Layout.styles.tsx
+++ components/templates/Layout/Layout.styles.tsx
@@ -2,11 +2,21 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   width: 100%;
-  min-height: 100vh;
+  height: 100vh;
+
+  display: flex;
+  align-items: top;
+  justify-content: center;
+  box-sizing: border-box;
+  overflow-y: auto;
 `;
 
 export const Wrapper = styled.main`
   width: 100%;
   max-width: 75rem;
   height: 100%;
+
+  margin-top: 5rem;
+
+  box-sizing: border-box;
 `;
