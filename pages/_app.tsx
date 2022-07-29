c1beafdefb2f0e6cd5662ae9d38528c591ef54b1
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-07-29 11:26:46 +0900

    chore: favicon & title 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@emotion/react';
 import { Layout } from 'components';
@@ -5,12 +6,18 @@ import { GlobalStyle, theme } from '../styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>WooriMap</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 }
 
