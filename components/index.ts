a2686ae607cabf655c973b0656abae7a512a9069
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-07-30 22:02:06 +0900

    feat: 컴포넌트 이름 변경

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,11 @@
 // atoms
-
 export { Button } from './atoms/Button';
 export { default as Input } from './atoms/Input';
 export { Profile } from '@components/atoms/Profile';
-export { ImageViewer } from 'components/atoms/ImageViewer';
+export { ImageBox } from '@components/atoms/ImageBox';
 
 // molecules
+<<<<<<< HEAD
 export { default as TextInput } from './molecules/TextInput';
 
 // organisms
@@ -15,6 +15,14 @@ export { NavBar } from '@components/organisms/NavBar';
 =======
 export { ImageSection } from 'components/organisms/ImageSection';
 >>>>>>> cf7295e (feat: image section 구성)
+=======
+export { NavBar } from '@components/organisms/NavBar';
+export { ImageList } from '@components/molecules/ImageList';
+
+// organisms
+export { LoggedInSection } from '@components/molecules/LoggedInSection';
+export { ImageViewer } from '@components/organisms/ImageViewer';
+>>>>>>> 3595273 (feat: 컴포넌트 이름 변경)
 
 // templates
 export { Layout } from '@components/templates/Layout';
--- components/organisms/ImageViewer/ImageViewer.tsx
+++ components/organisms/ImageViewer/ImageViewer.tsx
@@ -2,15 +2,15 @@ import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { dummyImages } from 'utils';
 import { IImageSource } from 'types';
-import { ImageViewer, ImageViewerContainer } from 'components';
-import * as S from './ImageSection.styles';
+import { ImageBox, ImageList } from 'components';
+import * as S from './ImageViewer.styles';
 
 interface INextSources {
   selectedSource: string;
   nextSources: Array<IImageSource>;
 }
 
-export function ImageSection() {
+export function ImageViewer() {
   const [selectedImage, setSelectedImage] = useState<string>(dummyImages[0]);
   const [sources, setSources] = useState<Array<IImageSource>>(
     dummyImages.map((src: string, idx: number) => ({
@@ -47,12 +47,8 @@ export function ImageSection() {
 
   return (
     <S.Container>
-      <ImageViewer size="large" src={selectedImage} />
-      <ImageViewerContainer
-        size="medium"
-        sources={sources}
-        onClick={onSelectImage}
-      />
+      <ImageBox size="large" src={selectedImage} />
+      <ImageList size="medium" sources={sources} onClick={onSelectImage} />
     </S.Container>
   );
 }
