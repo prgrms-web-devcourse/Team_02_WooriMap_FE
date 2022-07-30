3fcf3dabc69c0cea5d0bc241194ef53e52c0bdc2
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-07-30 20:45:43 +0900

    feat: 이미지 선택 처리

--- components/molecules/ImageViewerContainer/ImageViewerContainer.tsx
+++ components/molecules/ImageViewerContainer/ImageViewerContainer.tsx
@@ -1,20 +1,28 @@
-import { nanoid } from 'nanoid';
+import { IImageSource } from 'types';
 import { ImageViewer } from 'components/atoms/ImageViewer';
 import * as S from './ImageViewerContainer.styles';
 
 interface IImageViewerContainerProps {
   size: string;
-  sources: Array<string>;
+  sources: Array<IImageSource>;
+  onClick: (key: string) => void;
 }
 
 export function ImageViewerContainer({
   size,
   sources,
+  onClick,
 }: IImageViewerContainerProps) {
   return (
     <S.Container>
-      {sources.map((src: string) => (
-        <ImageViewer key={nanoid()} size={size} src={src} />
+      {sources.map(({ key, src, isSelected }: IImageSource) => (
+        <ImageViewer
+          key={key}
+          size={size}
+          src={src}
+          isSelected={isSelected}
+          onClick={() => onClick(key)}
+        />
       ))}
     </S.Container>
   );
--- components/organisms/ImageSection/ImageSection.tsx
+++ components/organisms/ImageSection/ImageSection.tsx
@@ -1,13 +1,58 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import { dummyImages } from 'utils';
+import { IImageSource } from 'types';
 import { ImageViewer, ImageViewerContainer } from 'components';
-
 import * as S from './ImageSection.styles';
 
+interface INextSources {
+  selectedSource: string;
+  nextSources: Array<IImageSource>;
+}
+
 export function ImageSection() {
+  const [selectedImage, setSelectedImage] = useState<string>(dummyImages[0]);
+  const [sources, setSources] = useState<Array<IImageSource>>(
+    dummyImages.map((src: string, idx: number) => ({
+      key: nanoid(),
+      src,
+      isSelected: idx === 0,
+    })),
+  );
+
+  const onSelectImage = (key: string) => {
+    const { selectedSource, nextSources } = sources.reduce(
+      (acc: INextSources, curr: IImageSource) => {
+        if (curr.key === key) {
+          return {
+            selectedSource: curr.src,
+            nextSources: [...acc.nextSources, { ...curr, isSeleced: true }],
+          };
+        }
+
+        return {
+          ...acc,
+          nextSources: [...acc.nextSources, { ...curr, isSeleced: false }],
+        };
+      },
+      {
+        selectedSource: '',
+        nextSources: [],
+      },
+    );
+
+    setSelectedImage(selectedSource);
+    setSources(nextSources);
+  };
+
   return (
     <S.Container>
-      <ImageViewer size="large" src={dummyImages[0]} />
-      <ImageViewerContainer size="medium" sources={dummyImages} />
+      <ImageViewer size="large" src={selectedImage} />
+      <ImageViewerContainer
+        size="medium"
+        sources={sources}
+        onClick={onSelectImage}
+      />
     </S.Container>
   );
 }
