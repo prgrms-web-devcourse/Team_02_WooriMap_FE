558c972f5d327dc7499ba82beb2d23c96fe2b456
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-07-30 17:49:31 +0900

    feat: image viewer container 처리

--- components/molecules/ImageViewerContainer/ImageViewerContainer.styles.tsx
+++ components/molecules/ImageViewerContainer/ImageViewerContainer.styles.tsx
@@ -3,6 +3,9 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   height: fit-content;
   width: 100%;
+  max-width: 39rem;
 
   margin-top: 1rem;
+
+  display: flex;
 `;
