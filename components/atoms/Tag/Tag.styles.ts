956c1cd21ae23f83b6e84331dc4f165434ec8d90
sangjin149 <sangjin149@gmail.com>
2022-07-30 21:57:17 +0900

    feat: Tag Style 일부 수정 및 Tag 에 deletable Prop 추가

--- components/atoms/Tag/Tag.styles.ts
+++ components/atoms/Tag/Tag.styles.ts
@@ -1,12 +1,13 @@
 import styled from '@emotion/styled';
 
 export const Tag = styled.div`
+  display: inline-block;
   text-align: center;
   text-decoration: none;
   margin: 0;
 
   font-size: 16px;
-  padding: 8px 32px;
+  padding: 8px 16px;
   border-radius: 25px;
   border: 1px solid black;
 `;
--- components/atoms/Tag/Tag.tsx
+++ components/atoms/Tag/Tag.tsx
@@ -2,10 +2,15 @@ import * as S from './Tag.styles';
 
 interface ITagProps {
   children: React.ReactNode;
+  deletable?: boolean;
 }
 
-function Tag({ children, ...props }: ITagProps) {
+function Tag({ children, deletable, ...props }: ITagProps) {
   return <S.Tag {...props}>{children}</S.Tag>;
 }
+
+Tag.defaultProps = {
+  deletable: false,
+};
 
 export default Tag;
