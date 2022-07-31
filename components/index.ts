6bc1a0143d1a3d7b5846c7b97c30c8a471a7d8c9
Junhyuck Ko <mrbartrns@naver.com>
2022-07-31 22:33:09 +0900

    fix: conflict 해결

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,14 @@
 // atoms
-export { default as Button } from './atoms/Button';
+export { Button } from './atoms/Button';
 export { default as Input } from './atoms/Input';
+export { Profile } from './atoms/Profile';
+
 // molecules
 export { default as TextInput } from './molecules/TextInput';
 
 // organisms
 export { LoggedInSection } from '@components/molecules/LoggedInSection';
+export { NavBar } from '@components/organisms/NavBar';
 
 // templates
 export { Layout } from '@components/templates/Layout';
