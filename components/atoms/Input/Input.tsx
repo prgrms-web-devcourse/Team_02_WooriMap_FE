3cd2140eeeb822ed1dc8bc7d7fd4986c3b67e20b
woojerry <woojerry@naver.com>
2022-07-31 14:49:29 +0900

    feat: molecules/TextInput 구현

--- components/atoms/Input/Input.tsx
+++ components/atoms/Input/Input.tsx
@@ -1,10 +1,9 @@
-import { useState, useEffect } from 'react';
 import * as S from './Input.styles';
 
 interface IInputProps {
   name?: string;
   type?: string;
-  defaultValue?: string;
+  value?: string;
   placeholder?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   deleteAll?: () => void;
@@ -13,35 +12,19 @@ interface IInputProps {
 function Input({
   name,
   type,
-  defaultValue,
+  value,
   placeholder,
   onChange,
   deleteAll,
   ...styles
 }: IInputProps) {
-  const [inputValue, setInputValue] = useState('');
-
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-    if (onChange) onChange(e);
-  };
-
-  const deleteAllInput = () => {
-    setInputValue('');
-    // deleteAll(name);
-  };
-
-  useEffect(() => {
-    if (defaultValue) setInputValue(defaultValue);
-  }, [defaultValue]);
-
   return (
     <S.Input
       name={name}
       type={type}
       placeholder={placeholder}
-      value={inputValue}
-      onChange={onChangeInput}
+      value={value}
+      onChange={onChange}
       {...styles}
     />
   );
--- components/molecules/TextInput/TextInput.tsx
+++ components/molecules/TextInput/TextInput.tsx
@@ -1,29 +1,47 @@
+import { useState } from 'react';
 import deleteIcon from 'public/image/delete.svg';
 import * as S from './TextInput.styles';
 
 interface ITextInputProps {
-  onChange?: () => void;
   name: string;
   placeholder: string;
+  onChange?: () => void;
+  deleteAll?: (name: string) => void;
 }
 
 function TextInput({
   onChange,
   name,
   placeholder,
+  deleteAll,
   ...styles
 }: ITextInputProps) {
-  const deleteAll = (name: string) => {};
+  const [inputValue, setInputValue] = useState('');
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    if (onChange) onChange();
+  };
+
+  const onClickDeleteButton = () => {
+    setInputValue('');
+    if (deleteAll) deleteAll(name);
+  };
 
   return (
     <S.TextInputWrapper {...styles}>
-      <S.TextInput onChange={onChange} name={name} placeholder={placeholder} />
+      <S.TextInput
+        name={name}
+        value={inputValue}
+        placeholder={placeholder}
+        onChange={onChangeInput}
+      />
       <S.DeleteButton
         src={deleteIcon}
         alt="Delete All"
         width={16}
         height={16}
-        onClick={() => deleteAll(name)}
+        onClick={onClickDeleteButton}
       />
     </S.TextInputWrapper>
   );
