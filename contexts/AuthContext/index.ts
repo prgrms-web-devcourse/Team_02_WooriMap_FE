d725a37aa1c60a08fda8b0b10b4c0bb3aa27003c
Junhyuck Ko <mrbartrns@naver.com>
2022-07-31 19:36:13 +0900

    feat: AuthContext 구현

---
+++ contexts/AuthContext/index.ts
@@ -0,0 +1,1 @@
+export { default as AuthProvider, useAuthContext } from './AuthContext';
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@emotion/react';
 import { Layout } from 'components';
+import { AuthProvider } from 'contexts/AuthContext';
 import { GlobalStyle, theme } from '../styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -11,12 +12,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>WooriMap</title>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </AuthProvider>
     </>
   );
 }
