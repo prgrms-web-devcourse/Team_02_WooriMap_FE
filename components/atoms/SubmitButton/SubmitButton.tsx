3957f2e60130ce9e4b98f8124af3ad733983c331
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-08-02 21:37:14 +0900

    chore: 리뷰 반영

--- components/atoms/SubmitButton/SubmitButton.tsx
+++ components/atoms/SubmitButton/SubmitButton.tsx
@@ -1,4 +1,4 @@
-import * as S from './Submit.styles';
+import * as S from './SubmitButton.styles';
 
 export function SubmitButton({ text }: { text: string }) {
   return <S.SubmitButton type="submit" value={text} />;
--- components/molecules/TextInputWithLabel/TextInputWithLabel.tsx
+++ components/molecules/TextInputWithLabel/TextInputWithLabel.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import { ITextInputProps } from 'types';
 import { TextInput } from 'components';
 import * as S from './TextInputWithLabel.styles';
