0e52dcb48908fdaac7efb36a608969535a26fded
Junhyuck Ko <mrbartrns@naver.com>
2022-08-02 10:48:23 +0900

    feat: 로그인 페이지 기본 틀 구성

--- pages/auth/signin/index.tsx
+++ pages/auth/signin/index.tsx
@@ -1,30 +1,58 @@
-import { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Image from 'next/image';
-import { FormBackground, TextInput } from 'components';
+import { Button, FormBackground, TextInput } from 'components';
 import mainLogoVertical from 'public/image/main-logo-vertical.svg';
 import * as S from './Signin.styles';
 
-interface ILoginForm {
-  id: string;
-  password: string;
-}
+type LoginFormKeyType = 'email' | 'password';
+
+type LoginFormType = {
+  [key in LoginFormKeyType]: string;
+};
 
 function Signin() {
-  const [data, setData] = useState<ILoginForm>({ id: '', password: '' });
+  const [data, setData] = useState<LoginFormType>({ email: '', password: '' });
+  const changeValue = useCallback(
+    (key: LoginFormKeyType) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      setData((prev) => ({ ...prev, [key]: e.target.value }));
+    },
+    [],
+  );
+  const resetValue = useCallback((key: LoginFormKeyType) => {
+    setData((prev) => ({ ...prev, [key]: '' }));
+  }, []);
   return (
     <FormBackground>
-      <S.LogoWrapper justify="center">
+      {data.email}
+      <S.FlexWrapper justify="center" padding="2rem">
         <Image
           src={mainLogoVertical}
           alt="main-logo"
           width={240}
           height={145}
         />
-      </S.LogoWrapper>
-      <S.InputWrapper>
-        <TextInput />
-        <TextInput />
-      </S.InputWrapper>
+      </S.FlexWrapper>
+      <S.Wrapper>
+        <S.InputWrapper margin="2rem 0">
+          <TextInput
+            value={data.email}
+            onChange={changeValue('email')}
+            onClickButton={() => resetValue('email')}
+            className="input-wrapper"
+          />
+          <TextInput
+            value={data.password}
+            onChange={changeValue('password')}
+            onClickButton={() => resetValue('password')}
+            className="input-wrapper"
+          />
+        </S.InputWrapper>
+      </S.Wrapper>
+      <S.FooterWrapper margin="auto 0 0 0">
+        <Button size="large" type="button">
+          로그인
+        </Button>
+      </S.FooterWrapper>
     </FormBackground>
   );
 }
