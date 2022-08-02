f7800261ac5c6e05ad91dcc8f6a7cdee1c9aa52e
Junhyuck Ko <mrbartrns@naver.com>
2022-08-02 17:42:00 +0900

    feat: 자동 로그인 로직 구현

--- apis/InterceptorProvider.ts
+++ apis/InterceptorProvider.ts
@@ -1,12 +1,21 @@
 import React, { useEffect } from 'react';
+import { AxiosRequestConfig } from 'axios';
+import { IApiResponse } from 'types/api';
+import { IToken } from 'types/auth';
 import LocalStorage from 'utils/storage';
+import { useAuthContext } from 'contexts/AuthContext';
 import instance from './instance';
 
 interface IInterceptor {
   children: React.ReactElement | null;
 }
 
+interface IRetryAxiosInstanceConfig extends AxiosRequestConfig {
+  retry?: boolean;
+}
+
 function InterceptorProvider({ children }: IInterceptor) {
+  const { setUser } = useAuthContext();
   useEffect(() => {
     const requestInterceptor = instance.interceptors.request.use((_config) => {
       const token = LocalStorage.getItem<string>('accessToken', '');
@@ -15,18 +24,55 @@ function InterceptorProvider({ children }: IInterceptor) {
         config.headers = {};
       }
       config.headers.Authorization = token ? `Bearer ${token}` : '';
+      console.log(config);
       return config;
     });
 
     const responseInterceptor = instance.interceptors.response.use(
-      (config) => config,
-      (error) => error,
+      (config) => {
+        return config;
+      },
+      async (error) => {
+        const config = error.config as IRetryAxiosInstanceConfig;
+        if (!error.response) return Promise.reject(error);
+        if (error.response.status === 401) return Promise.reject(error);
+        if (
+          config.url === '/auth/token' ||
+          config.url === '/auth/sigin' ||
+          config.url === '/fake/signin'
+        )
+          return Promise.reject(error);
+        if (!config.retry) {
+          config.retry = true;
+          return instance
+            .post<IApiResponse<IToken>>('/auth/token')
+            .then((response) => response.data.data)
+            .then((token) => {
+              const { accessToken } = token;
+              LocalStorage.setItem('accessToken', accessToken);
+              if (!config.headers) {
+                config.headers = {};
+              }
+              config.headers.Authorization = `Bearer ${accessToken}`;
+              return instance(config);
+            })
+            .catch(() => {
+              setUser(null);
+              if (!config.headers) {
+                config.headers = {};
+              }
+              config.headers.Authorization = '';
+              return instance(config);
+            });
+        }
+        return Promise.reject(error);
+      },
     );
     return () => {
       instance.interceptors.request.eject(requestInterceptor);
       instance.interceptors.response.eject(responseInterceptor);
     };
-  }, []);
+  }, [setUser]);
   return children;
 }
 
--- apis/instance.ts
+++ apis/instance.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: true,
 });
 
 export default instance;
