54814581ec832f1314ae50c0bda80f53b8ec16a5
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-08-04 08:41:44 +0900

    styls: 이미지 반영 및 스타일 수정

--- components/atoms/SearchBar/SearchBar.styles.tsx
+++ components/atoms/SearchBar/SearchBar.styles.tsx
@@ -5,9 +5,12 @@ export const Container = styled.div<{ isSearching: boolean }>`
 
   width: 392px;
   height: 48px;
-  padding: 12px 16px 11px 16px;
-  background-color: #fff;
 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #fff;
   border: 1px solid #e6e6e6;
   box-shadow: rgba(60, 64, 67, 0.3) 0 1px 2px,
     rgba(60, 64, 67, 0.15) 0 2px 6px 2px;
@@ -22,10 +25,7 @@ export const Container = styled.div<{ isSearching: boolean }>`
 
 export const Input = styled.input`
   margin: 0;
-
-  font-family: inherit;
-  font-weight: inherit;
-  list-style: none;
+  width: 90%;
 
   background-color: transparent;
   border-radius: 0;
@@ -33,129 +33,29 @@ export const Input = styled.input`
   border-width: 0;
   outline: 0;
 
+  font-family: inherit;
+  font-weight: inherit;
+  list-style: none;
+
   overflow: visible;
 `;
 
 export const SearchResultBox = styled.ul`
-  background-color: #fff;
-  border-radius: 0 0 8px 8px;
-  box-shadow: rgba(0, 0, 0, 0.2) 0 2px 4px;
-  font-size: 15px;
-  overflow: hidden;
-  padding: 8px 0;
+  position: relative;
+
   width: 392px;
   height: 300px;
 
-  /* left: 0;
-  bottom: 100px; */
+  padding: 8px 0;
 
-  position: relative;
+  background-color: #fff;
+  border-radius: 0 0 8px 8px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0 2px 4px;
+
+  font-size: 15px;
+  overflow: hidden;
 
   &::-webkit-scrollbar {
     display: none;
   }
 `;
-
-/* ".inherited-styles-for-exported-element" is a generated class for the inherited styles of the exported element, feel free to rename it. */
-
-// ::-webkit-input-placeholder {
-//     color: #999;
-//   }
-
-//   .inherited-styles-for-exported-element {
-//     white-space: normal;
-//   }
-
-//   [dir=ltr] {
-//     unicode-bidi: isolate;
-//   }
-
-//   .DAdBuc {
-//     background-color: #fff;
-//     border-radius: 0 0 8px 8px;
-//     box-shadow: rgba(0, 0, 0, .2) 0 2px 4px;
-//     font-size: 15px;
-//     overflow: hidden;
-//     padding: 8px 0;
-//     width: 392px;
-//   }
-
-//   @media only screen and (max-width: 408px) {
-//     .DAdBuc {
-//       width: calc(100% - 16px);
-//     }
-//   }
-
-//   .ZHeE1b {
-//     border-style: none;
-//     color: #8c8c8c;
-//     cursor: pointer;
-//     direction: ltr;
-//     font-size: 12px;
-//     min-height: 24px;
-//     padding-bottom: 7px;
-//     padding-top: 6px;
-//     position: relative;
-//     text-align: left;
-//   }
-
-//   .j2RmEf {
-//     line-height: 24px;
-//   }
-
-//   .l0wghb {
-//     display: flex;
-//     padding-right: 13px;
-//   }
-
-//   .DgCNMb {
-//     -webkit-box-flex: 1;
-//     flex: 1 1 0;
-//     overflow: hidden;
-//     text-overflow: ellipsis;
-//     white-space: nowrap;
-//   }
-
-//   .hCgzhd {
-//     float: left;
-//     height: 20px;
-//     margin: 2px 26px 0 18px;
-//     width: 20px;
-//   }
-
-//   .b9DfJb {
-//     color: #3c4043;
-//     font-size: 13px;
-//   }
-
-//   .EmLCAe {
-//     color: #70757a;
-//   }
-
-//   :focus {
-//     outline: 0;
-//   }
-
-//   .DgCNMb span {
-//     unicode-bidi: normal;
-//   }
-
-//   .ZHeE1b:hover {
-//     background-color: rgba(0, 0, 0, .06);
-//     outline: 0;
-//   }
-
-//   @media screen and (forced-colors:active) {
-//     .ZHeE1b:hover {
-//       border: 3px solid transparent;
-//     }
-//   }
-
-//   .sbsb_c:last-child .ZHeE1b {
-//     border-radius: 0 0 2px 2px;
-//   }
-
-//   .sbsb_c[dir=ltr] .DgCNMb {
-//     direction: ltr;
-//     text-align: left;
-//   }
--- components/atoms/SearchBar/SearchBar.tsx
+++ components/atoms/SearchBar/SearchBar.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import search from 'public/image/search.svg';
 import * as S from './SearchBar.styles';
 
 export function SearchBar() {
@@ -5,6 +7,7 @@ export function SearchBar() {
     <>
       <S.Container isSearching>
         <S.Input />
+        <Image src={search} width={16} height={16} alt="search" />
       </S.Container>
       <S.SearchResultBox />
     </>
