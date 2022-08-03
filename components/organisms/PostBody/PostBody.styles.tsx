e7853f45b2e4955ee8f4f1e41e5798a4c2deb9fd
sangjin149 <sangjin149@gmail.com>
2022-08-03 10:11:01 +0900

    feat: Map을 제외한 컴포넌트 배치

---
+++ components/organisms/PostBody/PostBody.styles.tsx
@@ -0,0 +1,64 @@
+import { Button } from 'components';
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 44rem;
+  border-top: 2px solid black;
+
+  margin: 10rem; //나중에 반드시 지울 것
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 1rem 0;
+`;
+
+export const TitleLine = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+
+export const Title = styled.h1`
+  font-size: 3.75rem;
+`;
+
+export const Date = styled.h6`
+  font-size: 1rem;
+  color: ${({ theme }) => theme.colors.gray};
+`;
+
+export const PostControl = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const EditPostButton = styled(Button)`
+  border-radius: 1.5rem;
+`;
+
+export const DeletePostLink = styled.a`
+  margin-top: 1rem;
+  font-size: 1rem;
+  color: ${({ theme }) => theme.colors.gray};
+  text-align: right;
+`;
+
+export const PostTags = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 2rem;
+  margin: 1rem 0;
+  overflow: auto;
+`;
+
+export const PostContent = styled.div`
+  width: 100%;
+  height: 18rem;
+  margin: 1rem 0;
+  font-size: 1.25rem;
+  overflow-y: auto;
+`;
---
+++ components/organisms/PostBody/PostBody.tsx
@@ -0,0 +1,36 @@
+import { TagList } from 'components';
+import * as S from './PostBody.styles';
+
+interface ITag {
+  tagName: string;
+  tagColor: string;
+  deletable?: boolean;
+}
+
+interface IPostBodyProps {
+  title: string;
+  date: string;
+  tagList: ITag[];
+  content: string;
+}
+
+export function PostBody({ title, date, tagList, content }: IPostBodyProps) {
+  return (
+    <S.Container>
+      <S.Header>
+        <S.TitleLine>
+          <S.Title>{title}</S.Title>
+          <S.Date>{date}</S.Date>
+        </S.TitleLine>
+        <S.PostControl>
+          <S.EditPostButton size="medium">포스트 편집</S.EditPostButton>
+          <S.DeletePostLink>포스트 삭제</S.DeletePostLink>
+        </S.PostControl>
+      </S.Header>
+      <S.PostTags>
+        <TagList tagList={tagList} />
+      </S.PostTags>
+      <S.PostContent>{content}</S.PostContent>
+    </S.Container>
+  );
+}
