a48fa1a0454b248f6beb6ec741a5dd680c3937a1
sangjin149 <sangjin149@gmail.com>
2022-08-05 16:56:02 +0900

    feat: 새 Tag 에 대하여 임의의 color을 부여하는 기능 추가

--- components/molecules/TagInput/TagInput.tsx
+++ components/molecules/TagInput/TagInput.tsx
@@ -1,6 +1,7 @@
-// 2. 임의의 색깔 부여
-// 4. css
-// 5. Deletable TagList 넣기
+// 1. 임의의 색깔 부여, 후에 tag.styles고치기
+// 2. 외부 클릭시 DropDown 닫히게
+// 3. 리팩토링- 로직, 컴포넌트 나누기 등
+// 2. css
 
 import { ITextInputProps, ITag } from 'types';
 import React, { useState } from 'react';
@@ -28,6 +29,17 @@ interface ITagInputProps extends ITextInputProps {
   tags: ITag[];
   tagData: ITag[];
 }
+
+const defaultTagColors: string[] = [
+  'rgba(217, 217, 217, 1)',
+  'rgba(217, 217, 217, 0.5)',
+  'rgba(255, 143, 143, 1)',
+  'rgba(255, 143, 143, 0.5)',
+  'rgba(151, 186, 255, 1)',
+  'rgba(151, 186, 255, 0.5)',
+  'rgba(255, 234, 122, 1)',
+  'rgba(255, 234, 122, 0.5)',
+];
 
 export function TagForm({
   name,
@@ -79,11 +91,12 @@ export function TagInput({ name, text, error, tags, tagData }: ITagInputProps) {
     e.preventDefault();
     const tagName = e.currentTarget.elements.tagName.value;
     e.currentTarget.elements.tagName.value = '';
+    const tagColor = defaultTagColors[Math.floor(Math.random() * 8)];
 
     if (value.some((tag) => tag.name === tagName)) return;
 
     const newValue: ITag[] = [...value];
-    const newTag = { name: tagName, color: '#FFFF00' };
+    const newTag = { name: tagName, color: tagColor };
     tagData.forEach((tagInfo) => {
       if (newTag.name === tagInfo.name) {
         newTag.color = tagInfo.color;
