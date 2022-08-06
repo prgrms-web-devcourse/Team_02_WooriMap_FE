7602019781eb9529f98d42dbea8f69214a936e28
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-08-06 15:45:30 +0900

    feat: 취소 버튼 시, 돌아가기 처리

--- components/templates/PostTemplate/PostTemplate.tsx
+++ components/templates/PostTemplate/PostTemplate.tsx
@@ -1,23 +1,30 @@
+import { useRouter } from 'next/router';
 import { Button } from 'components';
 import * as S from './PostTemplate.styles';
+
+interface IPostTemplateProps {
+  type: 'create' | 'edit';
+  onSubmit: (e: React.FormEvent<Element>) => Promise<void>;
+  imageSection: React.ReactNode;
+  contentSection: React.ReactNode;
+}
 
 export function PostTemplate({
   type,
   onSubmit,
   imageSection,
   contentSection,
-}: {
-  type: 'create' | 'edit';
-  onSubmit: (e: React.FormEvent<Element>) => Promise<void>;
-  imageSection: React.ReactNode;
-  contentSection: React.ReactNode;
-}) {
+}: IPostTemplateProps) {
+  const router = useRouter();
+
+  const onCancel = () => router.back();
+
   return (
     <S.Container onSubmit={onSubmit} id="post-write">
       <S.ImageSection>{imageSection}</S.ImageSection>
       <S.ContentSection>{contentSection}</S.ContentSection>
       <S.Wrapper>
-        <Button id="post-write" size="small">
+        <Button id="post-write" size="small" onClick={onCancel}>
           취소
         </Button>
         <Button id="post-write" size="medium" variant="black">
