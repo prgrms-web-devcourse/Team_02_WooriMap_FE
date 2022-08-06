ab3a0136ce9ec3836d04256318fd6ea1d7f94236
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-08-06 15:37:05 +0900

    refactor: 임시 데이터 helper 로 분리

--- components/organisms/PostWrite/PostWrite.tsx
+++ components/organisms/PostWrite/PostWrite.tsx
@@ -1,38 +1,7 @@
-import { nanoid } from 'nanoid';
 import { TextInputWithLabel, SearchableMap } from 'components';
 import { IPostFormState, IFormInputProps } from 'types';
+import { inputList } from './helper';
 import * as S from './PostWrite.styles';
-
-interface Itemp {
-  id: string;
-  text: string;
-  name: string;
-  variant: 'input' | 'calendar' | 'textarea' | 'tag';
-  placeholder?: string;
-}
-
-const temp: Array<Itemp> = [
-  {
-    id: nanoid(),
-    text: '제목',
-    name: 'title',
-    variant: 'input',
-    placeholder: '제목을 입력해주세요',
-  },
-  {
-    id: nanoid(),
-    text: '날짜',
-    name: 'date',
-    variant: 'calendar',
-  },
-  {
-    id: nanoid(),
-    text: '내용',
-    name: 'content',
-    variant: 'textarea',
-    placeholder: '우리의 이야기를 입력해주세요',
-  },
-];
 
 type IPostWrite = Omit<IPostFormState, 'imageUrls'>;
 
@@ -50,7 +19,7 @@ export function PostWrite({
 
   return (
     <S.Container>
-      {temp.map((item) => (
+      {inputList.map((item) => (
         <TextInputWithLabel
           key={item.id}
           text={item.text}
