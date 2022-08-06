12d0849ddbd474b1e1fc1cffa1826c47834a50e9
Junhyuck Ko <mrbartrns@naver.com>
2022-08-06 14:00:40 +0900

    chore: 불필요한 파일 제거

--- apis/auth.ts
+++ apis/auth.ts
@@ -1,9 +1,6 @@
 import { AxiosError } from 'axios';
 import instance from 'apis/instance';
-import { IApiResponse } from 'types/api';
 import { IInputState, ISingnUpRes } from 'types';
-import LocalStorage from 'utils/storage';
-import { ITokenSet, IRetryAxiosInstanceConfig } from 'types/auth';
 
 export async function signup({
   values,
@@ -40,42 +37,4 @@ export async function signup({
       message: '서버에러',
     };
   }
-}
-
-export async function getNewAccessToken() {
-  try {
-    const accessToken = await instance
-      .post<IApiResponse<ITokenSet>>('/fake/token', {
-        refreshToken: LocalStorage.getItem<string>('refreshToken', ''),
-      })
-      .then((response) => response.data.data.accessToken);
-    return accessToken;
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
-
-export function getConfigWithAuthorizedHeadersBy(
-  _config: IRetryAxiosInstanceConfig,
-) {
-  const config = { ..._config };
-  if (!config.headers) {
-    config.headers = {};
-  }
-  config.headers.Authorization = `Bearer ${LocalStorage.getItem<string>(
-    'accessToken',
-    '',
-  )}`;
-  return config;
-}
-
-export function isAuthorization(url: string | undefined) {
-  const authorizationUrlList = [
-    '/auth/token',
-    '/auth/login',
-    '/fake/token',
-    '/fake/signin',
-  ];
-  if (!url) return false;
-  return authorizationUrlList.includes(url);
 }
