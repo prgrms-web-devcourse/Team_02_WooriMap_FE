c8977d98f7f0e3f7800200515ad35608b2315b74
Junhyuck Ko <mrbartrns@naver.com>
2022-08-07 21:24:03 +0900

    chore: 원래대로 되돌리기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,4 @@
-import { useAuthContext } from 'contexts/AuthContext';
-import { useCallback, useEffect } from 'react';
-import { useAxiosInstance } from 'hooks';
-import api from 'apis/instance';
-
 function Home() {
-  const instance = useAxiosInstance();
-  const [user] = useAuthContext();
-
-  const onClick = useCallback(async () => {
-    try {
-      const response = await instance.post('/auth/token');
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }, [instance]);
-
-  useEffect(() => {
-    (async () => {
-      const response = await api.get('/fake/cookie');
-      console.log(response);
-    })();
-  }, []);
-  return (
-    <div>
-      <div>{JSON.stringify(user)}</div>
-      <button type="button" onClick={onClick}>
-        클릭
-      </button>
-    </div>
-  );
+  return <div />;
 }
 export default Home;
