768724bd2d0b4be1889635944bfebc88fbfa0a3c
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-08-08 18:30:30 +0900

    fix: fetch 에러 수정

--- components/organisms/ImageUploader/helper.ts
+++ components/organisms/ImageUploader/helper.ts
@@ -7,34 +7,17 @@ export const fetchFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
 
     formData.append('file', file);
 
-    const response = await fetch('https://dev.woorimap.p-e.kr/api/image', {
-      method: 'POST',
-      body: formData,
-      headers: {
-        'Content-Type': 'multipart/form-data',
+    formData.append('upload_preset', 'my-uploads');
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/dq4j0pffj/image/upload',
+      {
+        method: 'POST',
+        body: formData,
       },
-    })
-      .then((res) => {
-        console.log(res);
-        return res.json();
-      })
-      .catch((err) => console.log(err));
+    );
 
-    console.log(response);
-
-    // const data = await response.json();
-
-    // formData.append('upload_preset', 'my-uploads');
-
-    // const res = await fetch(
-    //   'https://api.cloudinary.com/v1_1/dq4j0pffj/image/upload',
-    //   {
-    //     method: 'POST',
-    //     body: formData,
-    //   },
-    // );
-
-    // const data = await res.json();
+    const data = await res.json();
 
     return {
       name,
