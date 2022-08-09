e280cf23360651837d72811a41a63b4cca8a90f1
Junhyuck Ko <mrbartrns@naver.com>
2022-08-09 14:52:40 +0900

    feat: AuthContext제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@emotion/react';
 import { RecoilRoot } from 'recoil';
 import { Layout } from 'components';
-import { AuthProvider } from 'contexts/AuthContext';
 import { GlobalStyle, theme } from '../styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -13,16 +12,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>WooriMap</title>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-      <AuthProvider>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <RecoilRoot>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </RecoilRoot>
-        </ThemeProvider>
-      </AuthProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </ThemeProvider>
     </>
   );
 }
