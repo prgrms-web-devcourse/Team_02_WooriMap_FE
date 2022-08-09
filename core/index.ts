2b2a516c0f01df4acce236066aab319a532b1ee2
Junhyuck Ko <mrbartrns@naver.com>
2022-08-09 11:42:02 +0900

    feat: initial recoil state 구성

--- core/index.ts
+++ core/index.ts
@@ -1,9 +1,22 @@
-import { atom } from 'recoil';
+import LocalStorage from 'utils/storage';
+import { atom, AtomEffect } from 'recoil';
 import { IUserResponse } from 'types/auth';
 
-const userState = atom<IUserResponse | null>({
+const LocalStorageEffect: AtomEffect<string | null> = ({ setSelf, onSet }) => {
+  const savedValue = LocalStorage.getItem<string, null>('accessToken', null);
+  setSelf(savedValue);
+
+  onSet((newValue, _, isReset) =>
+    isReset
+      ? LocalStorage.removeItem('accessToken')
+      : LocalStorage.setItem('accessToken', newValue),
+  );
+};
+
+const userState = atom<string | null>({
   key: 'user',
   default: null,
+  effects: [LocalStorageEffect],
 });
 
 export default userState;
