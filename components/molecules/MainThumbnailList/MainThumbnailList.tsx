34b391f3bef977bb81125cb90e0d8eef80125f6c
Owall <97934878+MayOwall@users.noreply.github.com>
2022-08-10 13:41:16 +0900

    refactor : ThumbnailList props refactor

--- components/molecules/MainThumbnailList/MainThumbnailList.tsx
+++ components/molecules/MainThumbnailList/MainThumbnailList.tsx
@@ -6,25 +6,14 @@ function ThumbnailCard({
   postThumbnailPath,
   title,
   createDate,
-  latitude,
-  longitude,
 }: IThumbnailCardProps) {
   const onClick = (_postId: string) => {
     // postId별 라우팅 관련 로직 추가 예정
     alert(`${_postId}를 클릭하셨군여 (๑•̀ㅂ•́)و✧`);
   };
 
-  const coordinate = {
-    latitude: latitude || null,
-    longitude: longitude || null,
-  };
-
   return (
-    <S.CardContainer
-      url={postThumbnailPath}
-      data-coordinate={coordinate}
-      onClick={() => onClick(postId)}
-    >
+    <S.CardContainer url={postThumbnailPath} onClick={() => onClick(postId)}>
       <div>
         <S.Title>{title}</S.Title>
         <S.CreateDate>{createDate}</S.CreateDate>
@@ -40,26 +29,15 @@ export function MainThumbnailList({
 }) {
   return (
     <S.MainThumbnailListContainer>
-      {postList.map(
-        ({
-          postId,
-          postThumbnailPath,
-          title,
-          createDate,
-          latitude,
-          longitude,
-        }) => (
-          <ThumbnailCard
-            key={postId}
-            postId={postId}
-            postThumbnailPath={postThumbnailPath}
-            title={title}
-            createDate={createDate}
-            latitude={latitude}
-            longitude={longitude}
-          />
-        ),
-      )}
+      {postList.map(({ postId, postThumbnailPath, title, createDate }) => (
+        <ThumbnailCard
+          key={postId}
+          postId={postId}
+          postThumbnailPath={postThumbnailPath}
+          title={title}
+          createDate={createDate}
+        />
+      ))}
     </S.MainThumbnailListContainer>
   );
 }
