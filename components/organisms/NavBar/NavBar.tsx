f17cf40381e4fab190e9bd13749be02ecd7cc90d
sangjin149 <sangjin149@gmail.com>
2022-08-12 21:47:56 +0900

    feat: Header 로고에 '/' 링크 추가

--- components/organisms/NavBar/NavBar.tsx
+++ components/organisms/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { LoggedInSection } from 'components';
 import mainLogo from 'public/image/main-logo.svg';
 import * as S from './NavBar.styles';
@@ -7,7 +8,15 @@ export function NavBar() {
   return (
     <S.Container>
       <S.Wrapper>
-        <Image src={mainLogo} alt="main-logo" width={240} height={40} />
+        <Link href="/">
+          <Image
+            src={mainLogo}
+            style={{ cursor: 'pointer' }}
+            alt="main-logo"
+            width={240}
+            height={40}
+          />
+        </Link>
         <LoggedInSection />
       </S.Wrapper>
     </S.Container>
