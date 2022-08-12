5a0a5d3174fa77aa56aa74c09482fcd3c62fd7ad
sangjin149 <sangjin149@gmail.com>
2022-08-12 20:08:20 +0900

    feat: Modal 컴포넌트 기본 기능 작성

---
+++ components/molecules/Modal/Modal.tsx
@@ -0,0 +1,32 @@
+import { HTMLAttributes, useEffect, useMemo } from 'react';
+import { useClickAway } from 'hooks';
+import ReactDOM from 'react-dom';
+import * as S from './Modal.styles';
+
+interface IModalProps extends HTMLAttributes<HTMLDivElement> {
+  isVisible: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+export function Modal({ isVisible, onClose, children, ...props }: IModalProps) {
+  const ref = useClickAway<HTMLDivElement>(() => {
+    onClose();
+  });
+  const element = useMemo(() => document.createElement('div'), []);
+  useEffect(() => {
+    document.body.appendChild(element);
+    return () => {
+      document.body.removeChild(element);
+    };
+  });
+
+  return ReactDOM.createPortal(
+    <S.BackgroundDim isVisible={isVisible}>
+      <S.ModalContainer ref={ref} {...props}>
+        {children}
+      </S.ModalContainer>
+    </S.BackgroundDim>,
+    element,
+  );
+}
