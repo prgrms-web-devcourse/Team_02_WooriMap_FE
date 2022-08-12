2ee834987daeaca1e4f95fb65548503a7c818c9a
sangjin149 <sangjin149@gmail.com>
2022-08-12 14:38:24 +0900

    fix: TagInputWithList의 불필요한 console.log 제거

--- components/molecules/TagInputWithList/TagInputWithList.tsx
+++ components/molecules/TagInputWithList/TagInputWithList.tsx
@@ -57,7 +57,7 @@ export function TagInputWithList({
         return Promise.reject(error);
       }
     };
-    console.log(getAllTags());
+    getAllTags();
   }, [instance]);
 
   const handleEnterType = (newTagName: string) => {
