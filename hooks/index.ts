550425b29218ddc852050f107fe0eb37a055f2d8
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-08-12 21:49:28 +0900

    feat: useTimeout 구성

--- hooks/index.ts
+++ hooks/index.ts
@@ -6,3 +6,4 @@ export { useComponentDidMount } from 'hooks/useComponentDidMount';
 export { useRecoilValueAfterMount } from 'hooks/useRecoilValueAfterMount';
 export { useClickAway } from 'hooks/useClickAway';
 export { useImage } from 'hooks/useImage';
+export { useTimeoutFn } from 'hooks/useTimeout';
