ecc25071746af2c9576d64f7c87708aea2e11057
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-08-13 15:33:13 +0900

    fix: nav bar ref warning 해결

--- components/organisms/NavBar/NavBar.tsx
+++ components/organisms/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
 import { LoggedInSection } from 'components';
 import { headerLogo } from 'public/image';
 import userState from 'core';
