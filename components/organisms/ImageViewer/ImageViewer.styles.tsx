b9dde7a09d20666f49d543b5665e4ae64b2529b5
Owall <97934878+MayOwall@users.noreply.github.com>
2022-08-13 17:28:01 +0900

    refactor : organism 단위 컴포넌트 스타일 수정

--- components/organisms/ImageViewer/ImageViewer.styles.tsx
+++ components/organisms/ImageViewer/ImageViewer.styles.tsx
@@ -1,12 +1,11 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.section`
-  height: fit-content;
-  width: 100%;
-  max-width: 37rem;
-
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  width: fit-content;
+  height: fit-content;
 `;
