da8607192204c51c4c906de4639cf707cc78f117
Owall <97934878+MayOwall@users.noreply.github.com>
2022-08-13 17:30:25 +0900

    refactor : template 단위 스타일 수정

--- components/templates/PostTemplate/PostTemplate.styles.tsx
+++ components/templates/PostTemplate/PostTemplate.styles.tsx
@@ -4,40 +4,66 @@ export const Container = styled.form`
   display: flex;
   flex-direction: column;
 
-  width: 100%;
-  height: 100%;
+  position: relative;
 
-  padding: 2rem 5%;
-
-  box-sizing: border-box;
+  width: 90rem;
+  height: 55rem;
 `;
 
 export const PostContent = styled.div`
   display: flex;
   justify-content: space-between;
+  gap: 2rem;
+
   width: 100%;
-  min-height: 55%;
+  height: 100%;
 `;
 
 export const ImageSection = styled.div`
-  width: 45%;
-  overflow: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+
+  border-radius: 1rem;
+
+  background-color: ${({ theme }) => theme.colors.whiteTransparent};
+  box-shadow: ${({ theme }) => theme.boxShadow.organism};
 `;
 
 export const ContentSection = styled.div`
-  width: 45%;
-  overflow: auto;
+  width: 100%;
+
+  padding: 4rem 3rem;
+
+  border-radius: 1rem;
+
+  background-color: ${({ theme }) => theme.colors.whiteTransparent};
+  box-shadow: ${({ theme }) => theme.boxShadow.organism};
 `;
 
 export const Wrapper = styled.div`
-  width: 100%;
-
   display: flex;
   justify-content: flex-end;
+
+  position: absolute;
+  bottom: 2rem;
+  right: 3rem;
+
+  width: 100%;
+
+  > button {
+    opacity: 70%;
+
+    transition: ${({ theme }) => theme.opacityTransition};
+
+    :hover {
+      opacity: 90%;
+    }
+  }
 
   & > button:first-of-type {
     margin-right: 1rem;
   }
-
-  grid-column: 1 / 3;
 `;
