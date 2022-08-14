7c8c7205499abe0aa86005ec77b5abb431d63f80
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-08-14 17:48:59 +0900

    fix: 스타일 수정

--- components/atoms/ToastItem/ToastItem.styles.ts
+++ components/atoms/ToastItem/ToastItem.styles.ts
@@ -80,9 +80,9 @@ export const ProgressBar = styled.span<IProgressBarProps>`
 
   height: 2px;
 
-  background-color: ${({ theme }) => theme.colors.white};
-  border-bottom-left-radius: 5px;
-  border-bottom-right-radius: 5px;
+  background-color: #fff;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
 
   animation-name: progress;
   animation-timing-function: linear;
