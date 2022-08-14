a68865475a0cd2c4b68cee7b8b7dd1e6467876d5
Owall <97934878+MayOwall@users.noreply.github.com>
2022-08-15 02:15:29 +0900

    refactor : 카카오맵 미리 로딩 방지 script 추가

--- components/templates/MainPageTemplate/MainPageTemplate.tsx
+++ components/templates/MainPageTemplate/MainPageTemplate.tsx
@@ -21,53 +21,55 @@ export function MainPageTemplate({
         postList={postList}
         handlePostFilter={handlePostFilter}
       />
-      <S.MapContainer>
-        <Map
-          width="100%"
-          height="100%"
-          center={{
-            lat: coordinate.latitude,
-            lng: coordinate.longitude,
-          }}
-        >
-          {postList.map((post) => (
-            <MapMarker
-              key={post.postId}
-              position={{
-                lat: Number(post.latitude),
-                lng: Number(post.longitude),
-              }}
-              clickable
-              onClick={() => {
-                setIsOverlayShown(true);
-                setSelectedMarker(post.postId);
-              }}
-              image={{
-                src: 'https://img1.daumcdn.net/thumb/R750x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FHzNaV%2FbtrJFPHq266%2FYSW64UbOvUPehLH6Kjo8bk%2Fimg.png',
-                size: {
-                  width: 40,
-                  height: 50,
-                },
-              }}
-            >
-              {isOverlayShown && selectedMarker === post.postId && (
-                <S.OverlayContainer
-                  onClick={() => {
-                    setIsOverlayShown(false);
-                  }}
-                >
-                  <MapMarkerOverlay
-                    postId={post.postId}
-                    postThumbnailPath={post.postThumbnailPath}
-                    title={post.title}
-                    createDate={post.createDate}
-                  />
-                </S.OverlayContainer>
-              )}
-            </MapMarker>
-          ))}
-        </Map>
-      </S.MapContainer>
+      {Map && (
+        <S.MapContainer>
+          <Map
+            width="100%"
+            height="100%"
+            center={{
+              lat: coordinate.latitude,
+              lng: coordinate.longitude,
+            }}
+          >
+            {postList.map((post) => (
+              <MapMarker
+                key={post.postId}
+                position={{
+                  lat: Number(post.latitude),
+                  lng: Number(post.longitude),
+                }}
+                clickable
+                onClick={() => {
+                  setIsOverlayShown(true);
+                  setSelectedMarker(post.postId);
+                }}
+                image={{
+                  src: 'https://img1.daumcdn.net/thumb/R750x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FHzNaV%2FbtrJFPHq266%2FYSW64UbOvUPehLH6Kjo8bk%2Fimg.png',
+                  size: {
+                    width: 40,
+                    height: 50,
+                  },
+                }}
+              >
+                {isOverlayShown && selectedMarker === post.postId && (
+                  <S.OverlayContainer
+                    onClick={() => {
+                      setIsOverlayShown(false);
+                    }}
+                  >
+                    <MapMarkerOverlay
+                      postId={post.postId}
+                      postThumbnailPath={post.postThumbnailPath}
+                      title={post.title}
+                      createDate={post.createDate}
+                    />
+                  </S.OverlayContainer>
+                )}
+              </MapMarker>
+            ))}
+          </Map>
+        </S.MapContainer>
+      )}
       <Link href="/post/write" passHref>
         <S.ButtonContainer>
           <S.NewPostButton variant="blackOutlined" size="medium">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,6 +23,16 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Noto+Serif+KR:wght@200;300;400;500;600;700;900&display=swap"
             rel="stylesheet"
           />
+          {/* KaKapMap 컴포넌트가 로딩되기 전에 렌더링 되는 것을 방지하는 script */}
+          {/* <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              kakao.maps.load(function() {
+                const  map = new kakao.maps.Map(node, options);
+              });
+                `,
+            }}
+          /> */}
         </Head>
         <body>
           <div id="toast-portal" />
