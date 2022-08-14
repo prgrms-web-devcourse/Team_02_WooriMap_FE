257b146eba0a468bd78473a48cf78f57c9a2ddcd
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-08-14 12:58:58 +0900

    feat: withSignInSignout 반영

--- hocs/withSigninSignout.tsx
+++ hocs/withSigninSignout.tsx
@@ -3,6 +3,10 @@ import { useRouter } from 'next/router';
 import { useComponentDidMount, useRecoilValueAfterMount } from 'hooks';
 import userState from 'core';
 
+/**
+ * NOTE: 해당 HOC는 auth/signin, auth/signup 등
+ * 로그인 하지 않은 페이지에서만 사용할 것
+ */
 function witihSigninSignout<P>(Component: FunctionComponent<P>) {
   return function WithAuthComponent(props: P) {
     const mounted = useComponentDidMount();
