25baa8031dbd68f25e1c5c0419df759207115ba5
Junhyuck Ko <mrbartrns@naver.com>
2022-08-15 01:09:43 +0900

    feat: 읽음 처리를 서버 api와 연결

--- hooks/useNotification/useNotification.ts
+++ hooks/useNotification/useNotification.ts
@@ -1,65 +1,72 @@
 import { useSSE } from 'hooks/useSSE';
 import { useCallback, useEffect, useState } from 'react';
-import { INotification } from 'types/notification';
-
-// TODO: dummydata 지울 것
-const dummyData: INotification[] = [
-  {
-    id: 1,
-    contentId: 40,
-    action: 'created',
-    nickName: 'yummyyummyyummy',
-    content: 'article 제목1',
-    isRead: false,
-  },
-  {
-    id: 2,
-    contentId: 21,
-    action: 'created',
-    nickName: 'yummy',
-    content: 'article 제목2',
-    isRead: false,
-  },
-  {
-    id: 3,
-    contentId: 18,
-    action: 'modified',
-    nickName: 'yummy',
-    content: 'article 제목3',
-    isRead: false,
-  },
-  {
-    id: 4,
-    contentId: 17,
-    action: 'modified',
-    nickName: 'yummy',
-    content: 'article 제목4',
-    isRead: false,
-  },
-  {
-    id: 5,
-    contentId: 16,
-    action: 'modified',
-    nickName: 'yummy',
-    content: 'article 제목5',
-    isRead: false,
-  },
-  {
-    id: 6,
-    contentId: 15,
-    action: 'created',
-    nickName: 'yummy',
-    content: 'article 제목6',
-    isRead: false,
-  },
-];
+import { INotification, NotificationResponseType } from 'types/notification';
+import { IApiResponse } from 'types/api';
+import { useAxiosInstance } from 'hooks';
 
 type ReturnTypes = [INotification[], (notificationId: number) => void];
 
 function useNotification(): ReturnTypes {
   const source = useSSE();
-  const [notifications, setNotifications] =
-    useState<INotification[]>(dummyData);
+  const instance = useAxiosInstance();
+  const [notifications, setNotifications] = useState<INotification[]>([]);
+
+  // ANCHOR: 읽지 않은 알림을 받아온다.
+  const getUnReadNotifications = useCallback(async () => {
+    try {
+      const unReadNotifications = await instance
+        .get<IApiResponse<NotificationResponseType[]>>('/notifications')
+        .then((response) => response.data.data);
+
+      setNotifications(
+        unReadNotifications.map((notification) => ({
+          ...notification,
+          isRead: false,
+        })),
+      );
+    } catch {
+      console.error(
+        '현재 알림을 불러올 수 없습니다. 새로고침하여 다시 불러오기를 시도하세요.',
+      );
+    }
+  }, [instance]);
+
+  // ANCHOR: 서버에서 읽음 처리
+  const onRead = useCallback(
+    async (notificationId: number) => {
+      try {
+        await instance.patch(`/notifications/${notificationId}`);
+      } catch {
+        console.error(
+          '서버 문제로 알림을 읽음 처리 하지 못했습니다. 다음에 다시 시도하세요.',
+        );
+      }
+    },
+    [instance],
+  );
+
+  // 읽음 처리
+  const readNotification = useCallback(
+    async (notificationId: number) => {
+      setNotifications((prev) =>
+        prev.map((notification) =>
+          notification.id === notificationId
+            ? { ...notification, isRead: true }
+            : notification,
+        ),
+      );
+
+      await onRead(notificationId);
+    },
+    [onRead],
+  );
+
+  /**
+   * initial load시 알림을 받아오는 부분
+   */
+  useEffect(() => {
+    getUnReadNotifications();
+  }, [getUnReadNotifications]);
 
   /**
    * 알림 이벤트 수신 부분
@@ -72,26 +79,6 @@ function useNotification(): ReturnTypes {
       console.log('sse:', e);
     });
   }, [source]);
-
-  /**
-   * TODO: useEffect로 처음 읽지 않음 알림을 받아오는 처리 실행
-   */
-
-  /**
-   * 읽음 처리
-   * TODO: API를 연결하여 서버에서도 읽음 처리를 할 수 있도록 해야 함
-   */
-  const readNotification = useCallback(
-    (notificationId: number) =>
-      setNotifications((prev) =>
-        prev.map((notification) =>
-          notification.id === notificationId
-            ? { ...notification, isRead: true }
-            : notification,
-        ),
-      ),
-    [],
-  );
 
   return [notifications, readNotification];
 }
