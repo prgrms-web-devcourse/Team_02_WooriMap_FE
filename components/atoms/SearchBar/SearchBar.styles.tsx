5a40b84bcfe207b7af9edfbc3e7c990a69eec34d
Owall <97934878+MayOwall@users.noreply.github.com>
2022-08-15 15:25:58 +0900

    refactor : 지도 검색창 모달 디자인 수정

--- components/atoms/SearchBar/SearchBar.styles.tsx
+++ components/atoms/SearchBar/SearchBar.styles.tsx
@@ -20,7 +20,7 @@ export const Wrapper = styled.div<{ isSearching: boolean }>`
   padding: 0 8px;
 
   background-color: #fff;
-  border: 1px solid ${({ theme }) => theme.colors.gray};
+  border: none;
   box-shadow: rgba(60, 64, 67, 0.3) 0 1px 2px,
     rgba(60, 64, 67, 0.15) 0 2px 6px 2px;
   border-bottom: 1px solid transparent;
@@ -38,8 +38,7 @@ export const Input = styled.input`
 
   background-color: transparent;
   border-radius: 0;
-  border-style: initial;
-  border-width: 0;
+  border: none;
   outline: 0;
 
   font-family: inherit;
@@ -56,17 +55,18 @@ export const Input = styled.input`
 export const SearchResultBox = styled.ul`
   position: relative;
 
-  width: 208px;
+  width: 10rem;
   max-height: 150px;
 
   padding: 8px 0;
 
   background-color: #fff;
   border-radius: 0 0 8px 8px;
-  box-shadow: rgba(0, 0, 0, 0.2) 0 2px 4px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0 4px 4px;
 
   font-size: 0.8rem;
   overflow: auto;
+  z-index: 300;
 
   &::-webkit-scrollbar {
     display: none;
@@ -87,6 +87,9 @@ export const SearchResult = styled.li`
   color: #8c8c8c;
 
   font-size: 0.8rem;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
 
   &:hover {
     background-color: rgba(0, 0, 0, 0.06);
