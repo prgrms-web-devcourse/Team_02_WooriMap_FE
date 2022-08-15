d7d1ba5e8415cde3f46af3ab13f0f6f8928a9345
Junhyuck Ko <mrbartrns@naver.com>
2022-08-15 23:06:20 +0900

    feat: d-day 계산 로직 수정

--- utils/calculatingDDay.ts
+++ utils/calculatingDDay.ts
@@ -2,8 +2,9 @@ export function calculatingDDay(date: string) {
   const startingDate = new Date(date);
   const currentDate = new Date();
 
-  const calculatedDay = Math.floor(
-    (currentDate.getTime() - startingDate.getTime()) / (1000 * 60 * 60 * 24),
+  return Math.floor(
+    Math.abs(
+      (currentDate.getTime() - startingDate.getTime()) / (1000 * 60 * 60 * 24),
+    ),
   );
-  return calculatedDay;
 }
