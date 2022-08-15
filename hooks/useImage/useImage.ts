1d05a6f158c1eb003536300c8b32a84f9a3e7699
Junhyuck Ko <mrbartrns@naver.com>
2022-08-16 00:00:33 +0900

    refactor: useImage 렌더링 최적화

--- hooks/useImage/useImage.ts
+++ hooks/useImage/useImage.ts
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useCallback, useMemo } from 'react';
 import { AxiosInstance } from 'axios';
 import { uploadImage } from 'apis/image';
 
@@ -6,30 +6,36 @@ export function useImage({ image }: { image: string | null }) {
   const ref = useRef<HTMLInputElement | null>(null);
   const [preview, setPreview] = useState<string | null>(image);
 
-  const onUpload = () => {
+  const onUpload = useCallback(() => {
     // 클릭하면 ref에 연결된 input을 엽니다.
     ref.current?.click() as unknown as HTMLInputElement;
-  };
+  }, []);
 
-  const onChange = async ({
-    e,
-    instance,
-  }: {
-    e: React.ChangeEvent<HTMLInputElement>;
-    instance: AxiosInstance;
-  }) => {
-    e.preventDefault();
+  const onChange = useCallback(
+    async ({
+      e,
+      instance,
+    }: {
+      e: React.ChangeEvent<HTMLInputElement>;
+      instance: AxiosInstance;
+    }) => {
+      e.preventDefault();
 
-    try {
-      const { data } = await uploadImage({ e, instance });
+      try {
+        const { data } = await uploadImage({ e, instance });
 
-      if (data) {
-        setPreview(data);
+        if (data) {
+          setPreview(data);
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error: unknown) {
-      console.error(error);
-    }
-  };
+    },
+    [],
+  );
 
-  return { ref, preview, onUpload, onChange };
+  return useMemo(
+    () => ({ ref, preview, onUpload, onChange }),
+    [onChange, onUpload, preview],
+  );
 }
