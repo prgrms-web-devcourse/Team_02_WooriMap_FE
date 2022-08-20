2dc5569380c491fc35e174c51af5d1d8cd0f5e7b
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-08-20 12:30:18 +0900

    feat: _document에 meta tag 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,12 +17,18 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="ko">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Noto+Serif+KR:wght@200;300;400;500;600;700;900&display=swap"
             rel="stylesheet"
           />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="우리맵" />
+          <meta property="og:title" content="우리맵" />
+          <meta property="og:description" content="우리 갔던곳, 우리맵" />
+          <meta property="og:image" content="https://i.imgur.com/ZzqdUZB.png" />
+          <meta property="og:url" content="https://woorimap.vercel.app" />
         </Head>
         <body>
           <div id="toast-portal" />
